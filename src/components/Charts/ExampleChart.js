3d2001f87484e1a40ab9556ae7c314a62ed46ad5
Okan <okanacioglu@gmail.com>
2022-12-21 20:26:52 +0300

    chart-API

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -1,86 +1,51 @@
-// STEP 1 - Include Dependencies
+// Include Dependencies
 // Include react
 import React from 'react'
 
-// Include the react-fusioncharts component
+//! belows is default export, we can name whatever we want...
+// Include the react-fusioncharts component --> //! first library
 import ReactFC from 'react-fusioncharts'
 
-// Include the fusioncharts library
+// Include the fusioncharts library --> //! second library
 import FusionCharts from 'fusioncharts'
 
-// Include the chart type
-import Column2D from 'fusioncharts/fusioncharts.charts'
+// Include the chart type --> //! chart that we are going to render
+import Chart from 'fusioncharts/fusioncharts.charts'
 
-// Include the theme as fusion
+// Include the theme as fusion --> //! chart theme
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion'
 
 // Adding the chart and theme as dependency to the core fusioncharts
-ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme)
+//! fcRoot --> is a method...
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme)
 
-// STEP 2 - Chart Data
-const chartData = [
-  {
-    label: 'Venezuela',
-    value: '290',
-  },
-  {
-    label: 'Saudi',
-    value: '260',
-  },
-  {
-    label: 'Canada',
-    value: '180',
-  },
-  {
-    label: 'Iran',
-    value: '140',
-  },
-  {
-    label: 'Russia',
-    value: '115',
-  },
-  {
-    label: 'UAE',
-    value: '100',
-  },
-  {
-    label: 'US',
-    value: '30',
-  },
-  {
-    label: 'China',
-    value: '30',
-  },
-]
-
-// STEP 3 - Creating the JSON object to store the chart configurations
-const chartConfigs = {
-  type: 'column2d', // The chart type
-  width: '700', // Width of the chart
-  height: '400', // Height of the chart
-  dataFormat: 'json', // Data type
-  dataSource: {
-    // Chart Configuration
-    chart: {
-      //Set the chart caption
-      caption: 'Countries With Most Oil Reserves [2017-18]',
-      //Set the chart subcaption
-      subCaption: 'In MMbbl = One Million barrels',
-      //Set the x-axis name
-      xAxisName: 'Country',
-      //Set the y-axis name
-      yAxisName: 'Reserves (MMbbl)',
-      numberSuffix: 'K',
-      //Set the theme for your chart
-      theme: 'fusion',
+// Creating the DOM element to pass the react-fusioncharts component
+const ChartComponent = ({ data }) => {
+  // Creating the JSON object to store the chart configurations
+  const chartConfigs = {
+    type: 'column2d', // The chart type --> //! This is where we change chart type...
+    width: '400', // Width of the chart --> //! will be made responsive
+    height: '400', // Height of the chart --> //! will be made responsive
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: 'Countries With Most Oil Reserves [2017-18]',
+        //Set the chart subcaption
+        subCaption: 'In MMbbl = One Million barrels',
+        //Set the x-axis name
+        xAxisName: 'Country',
+        //Set the y-axis name
+        yAxisName: 'Reserves (MMbbl)',
+        numberSuffix: 'K',
+        //Set the theme for your chart
+        theme: 'fusion',
+      },
+      // Chart Data
+      data,
     },
-    // Chart Data
-    data: chartData,
-  },
-}
-
-// STEP 4 - Creating the DOM element to pass the react-fusioncharts component
-const ChartComponent = () => {
+  }
   return <ReactFC {...chartConfigs} />
 }
 
