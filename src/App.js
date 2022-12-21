309b8e8bbd9d062452749ae0c9bc0e690b3b83dc
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-21 13:11:58 +0300

    add-react-router-and-setup-pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react'
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='*'>
+          <Error></Error>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
+//?? ------------- React Router -----------
+//! exact prop --> only navigate to Dashboard ("/") if it is exactly match the URL
+//! It is better user experience to setup a Error page if anyone type a URL and in that URL there is no related page in our app...
+//! For Error page (path='*') used. Means that whatever we write on the URL this is always match.
