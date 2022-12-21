69ebad400ca58d2d2e583b59bd42bc6bbbf0fef3
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-21 13:56:39 +0300

    context-api

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect, Children } from 'react'
+import mockUser from './mockData.js/mockUser'
+import mockRepos from './mockData.js/mockRepos'
+import mockFollowers from './mockData.js/mockFollowers'
+import axios from 'axios'
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = 'https://api.github.com'
+
+//! Create Context
+const GithubContext = React.createContext()
+
+//! Now we have access to two components --> Provider,Consumer --> but will just use Provider.
+
+//! We dont want to wrap our application directly with Provider because we want more logic so we create a new component...
+//! Whatever we pass in the value is gonna be accessible all throughout the app.
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+  )
+}
+
+//! We need to export both GithubContext and GithubProvider
+//! GithubContext --> in order to access data
+//! GithubProvider --> in order to wrap our app
+
+export { GithubProvider, GithubContext }
