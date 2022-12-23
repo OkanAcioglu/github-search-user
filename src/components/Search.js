f56f9d303d5d048160470ca8f6d6bcccbd9f6c23
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-23 13:49:18 +0300

    search-user

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,13 +4,14 @@ import { MdSearch } from 'react-icons/md'
 import { GithubContext } from '../context/context'
 const Search = () => {
   const [user, setUser] = React.useState('')
-  const { requests, error } = React.useContext(GithubContext)
+  const { requests, error, searchGithubUser } = React.useContext(GithubContext)
   //console.log(requests)
   // get things from global context
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (user) {
+      searchGithubUser(user)
       // there will be more logic
       // optinal
       //setUser('')
--- src/context/context.js
+++ src/context/context.js
@@ -23,6 +23,22 @@ const GithubProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true)
   //* Errors
   const [error, setError] = useState({ show: false, msg: '' })
+
+  //* Searching user functionality
+  const searchGithubUser = async (user) => {
+    //! everytime we start searching we wanna make sure we remove the error message if there is...
+    toggleError()
+    //* set loading true
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) => {
+      console.log(err)
+    })
+    if (response) {
+      setGithubUser(response.data)
+    } else {
+      toggleError(true, 'there is no user with that username')
+    }
+  }
+
   //* Check Requests
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
@@ -51,7 +67,14 @@ const GithubProvider = ({ children }) => {
   return (
     // //! ES6 syntax --> if the property name is equal to a variable name that has the value, we can write just the property name... (githubUser: githubUser === githubUser)
     <GithubContext.Provider
-      value={{ githubUser, repos, followers, requests, error }}
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+      }}
     >
       {children}
     </GithubContext.Provider>
