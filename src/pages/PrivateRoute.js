dbcc28cc182592fa154c99d15a749f0ba69ca7da
Okan <okanacioglu@gmail.com>
2022-12-29 00:45:17 +0300

    private-route-wrapper

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
 
-const PrivateRoute = () => {
-  return <h2>private route component</h2>;
-};
-export default PrivateRoute;
+//! Ones we logout, we kick back to login page. Eventhough we know that dashboard is essentially our homepage, if we trying to get there (since we do not logged in) we being kicked back to a login page. We only can access to the dashboard if we are login.
+//! Firstly we need to setup private root.
+//! Dashboard will be wrapped by private route.
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { isAuthenticated, user } = useAuth0()
+  const isUser = isAuthenticated && user
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return isUser ? children : <Redirect to='/login'></Redirect>
+      }}
+    ></Route>
+  )
+}
+export default PrivateRoute
