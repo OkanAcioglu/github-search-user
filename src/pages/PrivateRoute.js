a795dec97c852a2093e09095f7bbbfadb19057fb
Okan <okanacioglu@gmail.com>
2022-12-29 01:24:55 +0300

    refactor-to-react-router-6

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,21 +1,17 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import { Navigate } from 'react-router'
 import { useAuth0 } from '@auth0/auth0-react'
 
 //! Ones we logout, we kick back to login page. Eventhough we know that dashboard is essentially our homepage, if we trying to get there (since we do not logged in) we being kicked back to a login page. We only can access to the dashboard if we are login.
 //! Firstly we need to setup private root.
 //! Dashboard will be wrapped by private route.
 
-const PrivateRoute = ({ children, ...rest }) => {
+const PrivateRoute = ({ children }) => {
   const { isAuthenticated, user } = useAuth0()
   const isUser = isAuthenticated && user
-  return (
-    <Route
-      {...rest}
-      render={() => {
-        return isUser ? children : <Redirect to='/login'></Redirect>
-      }}
-    ></Route>
-  )
+  if (!isUser) {
+    return <Navigate to='/login' />
+  }
+  return children
 }
 export default PrivateRoute
